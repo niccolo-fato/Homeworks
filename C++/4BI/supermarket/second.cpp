c4871a5ef9af855eda2f07a0d9a6e9b382677296
niccolo <niccolofato13@gmail.com>
2021-02-19 16:16:17 +0100

    changes

--- C++/4BI/supermarket/second.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "second.h"
-using namespace std;
-
-void d(){
-    cout<<"AO MONNO\n";
-    return;
-}
--- C++/4BI/supermarket/second.h
+++
@@ -1,6 +0,0 @@
-#ifndef SECOND_H
-#define SECOND_H
-#include <iostream>
-
-void d();
-#endif //SECOND_H
