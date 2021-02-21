465fddd9ca64016aa3410b28b04c2bcd00302326
niccolo <niccolofato13@gmail.com>
2021-02-21 16:06:46 +0100

    Update script.js

--- Web/project1/script.js
+++ Web/project1/script.js
@@ -1,9 +1,17 @@
 function Consonants() {
-    var name = document.getElementById("inputName").value;
+    let name = document.getElementById("inputName").value;
+    let surname = document.getElementById("inputSurname").value;
     document.write("Le consonanti trovate nel nome sono: ")
-    for (var i = 0; i < name.length; i++) {
-            if (name.charAt(i) != "a" && name.charAt(i) != "e" && name.charAt(i) != "i" && name.charAt(i) != "o" && name.charAt(i) != "u") {
-                document.write(name.charAt(i))
-            }
+    for (let i = 0; i < name.length; i++) {
+        if (name[i] != "a" && name[i] != "e" && name[i] != "i" && name[i] != "o" && name[i] != "u") {
+            document.write(name[i])
         }
     }
+    document.write("<br> Le consonanti trovate nel cognome sono: ")
+    for (let i = 0; i < surname.length; i++) {
+        if (surname[i] != "a" && surname[i] != "e" && surname[i] != "i" && surname[i] != "o" && surname[i] != "u") {
+            document.write(surname[i])
+        }
+    }
+}
+
