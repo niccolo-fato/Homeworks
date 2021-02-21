804863c2f2968d05317ee6a277473d47684136e9
niccolo <niccolofato13@gmail.com>
2021-02-21 14:19:24 +0100

    Update script.js

--- Web/project1/script.js
+++ Web/project1/script.js
@@ -1,11 +1,10 @@
 function findConsonants(){
-    var name = document.f1.n1.value;
-    var vocali="aeiou"
+    var name = document.getElementById("inputName").value;
     var consonanti = ""
     for (var i=0; i<name.length; i++){
-        if(vocali.indexOf(name[i])!= -1){
+        if(name.charAt(i) != "a"){
             consonanti+= nome[i] + ","
         }
     }
-    document.getElementById("cons").innerHTML = "Le consonanti trovate sono: " + consonanti
+    document.write("Le consonanti trovate sono: " + consonanti)
 }
