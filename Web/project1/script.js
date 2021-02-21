a10fc4db29b1667fc309a74325e213a6598738dc
niccolo <niccolofato13@gmail.com>
2021-02-21 14:52:12 +0100

    Update script.js

--- Web/project1/script.js
+++ Web/project1/script.js
@@ -1,10 +1,16 @@
-function findConsonants(){
-    var name = document.getElementById("inputName").value;
-    var consonanti = ""
-    for (var i=0; i<name.length; i++){
-        if(name.charAt(i) != "a"){
-            consonanti+= nome[i] + ","
+function Consonants() {
+    var name = document.getElementById("inputName").value
+    var surname = surname = document.getElementByld("inputSurname").value
+    document.write("Le consonanti trovate nel nome sono: ")
+    for (var i = 0; i < name.length; i++) {
+            if (name.charAt(i) != "a" && name.charAt(i) != "e" && name.charAt(i) != "i" && name.charAt(i) != "o" && name.charAt(i) != "u") {
+                document.write(name.charAt(i))
+            }
         }
     }
-    document.write("Le consonanti trovate sono: " + consonanti)
-}
+    document.write("Le consonanti trovate nel cognome sono: ")
+    for (var i = 0; i < surname.length; i++) {
+            if (surname.charAt(i) != "a" && surname.charAt(i) != "e" && surname.charAt(i) != "i" && surname.charAt(i) != "o" && surname.charAt(i) != "u") {
+                document.write(surname.charAt(i))
+            }
+        }
