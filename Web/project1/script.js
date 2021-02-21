acddc09af5da1ee368a39cd747524f23ce371bd0
niccolo <niccolofato13@gmail.com>
2021-02-21 14:59:28 +0100

    Update script.js

--- Web/project1/script.js
+++ Web/project1/script.js
@@ -1,6 +1,5 @@
 function Consonants() {
-    var name = document.getElementById("inputName").value
-    var surname = surname = document.getElementByld("inputSurname").value
+    var name = document.getElementById("inputName").value;
     document.write("Le consonanti trovate nel nome sono: ")
     for (var i = 0; i < name.length; i++) {
             if (name.charAt(i) != "a" && name.charAt(i) != "e" && name.charAt(i) != "i" && name.charAt(i) != "o" && name.charAt(i) != "u") {
@@ -8,9 +7,3 @@ function Consonants() {
             }
         }
     }
-    document.write("Le consonanti trovate nel cognome sono: ")
-    for (var i = 0; i < surname.length; i++) {
-            if (surname.charAt(i) != "a" && surname.charAt(i) != "e" && surname.charAt(i) != "i" && surname.charAt(i) != "o" && surname.charAt(i) != "u") {
-                document.write(surname.charAt(i))
-            }
-        }
