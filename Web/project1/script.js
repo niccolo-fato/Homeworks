8e2235dc5301b88881339bc63c69059f7bc5f6b0
niccolo <niccolofato13@gmail.com>
2021-02-28 14:38:48 +0100

    Update script.js

--- Web/project1/script.js
+++ Web/project1/script.js
@@ -1,17 +1,26 @@
 function Consonants() {
     let name = document.getElementById("inputName").value;
     let surname = document.getElementById("inputSurname").value;
-    document.write("Le consonanti trovate nel nome sono: ")
-    for (let i = 0; i < name.length; i++) {
-        if (name[i] != "a" && name[i] != "e" && name[i] != "i" && name[i] != "o" && name[i] != "u") {
-            document.write(name[i])
+    let day = document.getElementById("day").value
+    let month = document.getElementById("month").value
+    let year = document.getElementById("year").value
+    document.write("Risultato: ")
+    let count = 0
+        for (let i = 0; i < name.length; i++) {
+            if (name[i] != "a" && name[i] != "e" && name[i] != "i" && name[i] != "o" && name[i] != "u" && count != 3) {
+                document.write(name[i])
+                count++
+            }
         }
-    }
-    document.write("<br> Le consonanti trovate nel cognome sono: ")
+    count = 0
     for (let i = 0; i < surname.length; i++) {
-        if (surname[i] != "a" && surname[i] != "e" && surname[i] != "i" && surname[i] != "o" && surname[i] != "u") {
+        if (surname[i] != "a" && surname[i] != "e" && surname[i] != "i" && surname[i] != "o" && surname[i] != "u" && count != 3) {
             document.write(surname[i])
+            count++
         }
     }
+    document.write(day)
+    document.write(month)
+    document.write(year)
 }
 
